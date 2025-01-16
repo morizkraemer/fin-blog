d16af4f4d327e6109bb21ceb68ea7c67d0f3b112
Moriz Kraemer <=>
2025-01-16 17:00:30 +0100

    refactor code to use async await

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,24 +1,21 @@
-import {Blog} from '../models/Blog.js'
+import { Blog } from '../models/Blog.js'
 import express from "express"
 
 const PostsRouter = express.Router()
 
-PostsRouter.get('/blogs', (req, res) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            res.json(blogs)
-        })
+PostsRouter.get('/blogs', async (req, res) => {
+    const blogs = await Blog.find({})
+    res.json(blogs)
 })
 
-PostsRouter.post('/blogs', (req, res) => {
+PostsRouter.post('/blogs', async (req, res, next) => {
     const newBlogEntry = new Blog(req.body)
-    newBlogEntry
-        .save()
-        .then(result => {
-            res.status(201).json(result)
-        })
+    try {
+        const blogEntry = await newBlogEntry.save()
+        res.status(201).json(blogEntry)
+    } catch (err) {
+        next(err)
+    }
 })
-
 
 export { PostsRouter }
