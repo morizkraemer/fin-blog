a4f8581cba89a32c3266bed1171b200980293b8f
Moriz Kraemer <=>
2025-01-17 18:55:10 +0100

    print errormessages based on dev mode

--- utils/logging.js
+++ utils/logging.js
@@ -1,11 +1,18 @@
+import env from './env-config.js'
+
 const info = (message) => {
     console.log('-- INFO: --');
     console.log(message);
 }
 
 const error = (errormessage) => {
-    console.log('-- ERRORHANDLER: --');
-    console.log(errormessage.name);
+    if (env.MODE === 'test') {
+        console.log('-- ERRORHANDLER: --');
+        console.log(errormessage.name);
+    } else {
+        console.log('-- ERRORHANDLER: --');
+        console.log(errormessage);
+    }
 }
 
 export default { info, error }
