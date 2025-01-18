727f745ae3aebdbd3354108231368d534a48a232
Moriz Kraemer <=>
2025-01-18 19:21:43 +0100

    clean up and errors

--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,15 @@ import bcrypt from 'bcrypt'
 const usersRouter = Router()
 
 usersRouter.post('/users', async (req, res, next) => {
-    try {const { username, name, password } = req.body
+    try {
+        const { username, name, password } = req.body
 
-    const passwordHash = await bcrypt.hash(password, 10)
+        const passwordHash = await bcrypt.hash(password, 10)
 
-    const newUser = new User({ username, name, passwordHash })
-    const savedUser = await newUser.save()
-    res.status(201).json(savedUser)} catch (err) {
+        const newUser = new User({ username, name, passwordHash })
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    } catch (err) {
         next(err)
     }
 })
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import app from './app.js'
 import env from './utils/env-config.js'
+import logging from './utils/logging.js'
 
 const PORT = env.PORT
 app.listen(PORT, () => {
